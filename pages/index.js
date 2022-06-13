06fe8178b0b871d988b6cd42831999a9f0c516fc
Marc Medhat <marcmedhat6211@gmail.com>
2022-06-13 16:56:51 +0200

    using the getStaticProps method to enable the SSG (Static Site Generation)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -20,21 +20,50 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
+const HomePage = (props) => {
   /**
    * Using the regular useEffect approach to fetch daa on page load will cause a problem with NextJS and the ssr feature
    * because the useEffect hook function runs AFTER the component is rendered
    * So typically what happens is that NextJS prerenders the page on the server and send back the html page
    * but that html page does not include the list rendered in the useEffect hook because it ran after the component was evaluated
    */
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  // const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  useEffect(() => {
-    //send a http request and fetch data
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
+  // useEffect(() => {
+  //   //send a http request and fetch data
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, []);
 
-  return <MeetupList meetups={loadedMeetups} />;
+  // return <MeetupList meetups={loadedMeetups} />;
+
+  /**
+   * After using the getStaticProps method, we now do not need the useEffect nor the state because our data is ready and sent through the props object
+   */
+  return <MeetupList meetups={props.meetups} />;
+};
+
+/**
+ * This method is only allowed in the "pages" firectory, in other words, in a "page" component
+ * The method has to be named "getStaticProps" -> reserved name
+ * This method allows NextJS pre render the fetched data and includes it in the html file on the server
+ * This method is called BEFORE the component method (in this case the HomePage method)
+ * This approach is called "Static Generation"
+ * This method is allowed to be asynchronous
+ * This code is always executed during the build process
+ *
+ * Always returns an object
+ * That object has to have a key named props (the props that component will receive as an argument)
+ *
+ * How it Works?
+ *    It waits until your data is loaded, and then it returns the props that is then used in the rendered component
+ */
+export const getStaticProps = async () => {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 };
 
 export default HomePage;
